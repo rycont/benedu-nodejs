387121e5b7aa3516d0b0f4239d0578bcd846db36
rycont <reactdev@kakao.com>
2020-10-03 10:31:09 +0900

    chore : multiplatform support

--- src/api.ts
+++ src/api.ts
@@ -1,29 +1,31 @@
-import fetch, { RequestInfo, RequestInit } from "node-fetch"
-import {parse as HTMLParser} from "fast-html-parser";
+import { parse as HTMLParser } from "fast-html-parser";
 import { token } from "./auth/getUserToken"
 import FormData from 'form-data'
 import getEnv from "./env"
 
+let fetch: any
+
+export const setFetch = (f: any) => fetch = f
+
 export default {
-  async get(uri: string, config?: RequestInit) {
+  async get(uri: string, formdata?: FormData) {
     return await fetch(getEnv('API_URI') + uri, {
-      ...config,
+      body: formdata,
       headers: {
-        ...(config?.headers),
+        ...(formdata && formdata.getHeaders()),
         Cookie: `ASP.NET_SessionId=${token}`
-      }})
+      },
+      method: 'POST'
+    })
   },
   async text(uri: string, config?: {
     [key: string]: string
   }) {
+    console.log(uri, config)
     if (!config) return (await this.get(uri)).text()
     const formdata = new FormData()
     Object.entries(config).forEach(([key, value]) => formdata.append(key, value))
-    return await (await this.get(uri, {
-      body: formdata,
-      method: 'POST',
-      headers: formdata.getHeaders()
-    })).text()
+    return await (await this.get(uri, formdata)).text()
   },
   async html(uri: string, config?: {
     [key: string]: string
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import getUserToken from './auth/getUserToken'
+import fetch from 'node-fetch'
+import api, { setFetch } from './api'
+import getUserToken, { token } from './auth/getUserToken'
 import getEnv from './env'
 import getCreatedExam from './exam/getCreatedExam'
 
+setFetch(fetch)
+
 getUserToken(getEnv('EMAIL'), getEnv('PASSWORD'))
-  .then(() => getCreatedExam())
+  .then(() => {
+    console.log(token)
+    return getCreatedExam()
+  })
   .then(console.log)
